ff4eb809be4f4dbdffa3d35382b9baafb76e4811
thelubemaster <35251219+thelubemaster@users.noreply.github.com>
2026-08-17 09:32:35 -0400

    Add vite.config.ts tsconfig.json index.html capacitor.config.ts workflows

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,8 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "app.schoolie.wiring3600",
   appName: "3600 Wiring",
-  webDir: "companion",
-  android: {
-    allowMixedContent: true,
-  },
+  webDir: "dist",
+  android: { allowMixedContent: true },
 };
 
 export default config;
